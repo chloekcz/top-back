3404b1e3fd053f7fe0af00c53f9bd8e84289230c
chloe <chloekaczmarek0@gmail.com>
2018-12-07 16:52:00 +0100

    votes

--- src/main/java/dev/top/controller/VoteController.java
+++ src/main/java/dev/top/controller/VoteController.java
@@ -3,8 +3,10 @@ package dev.top.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.top.entities.Vote;
@@ -18,5 +20,10 @@ public class VoteController {
 	@Autowired
 	private VoteRepo voteRepo;
 	
-
+	
+	@PostMapping("votes")
+	public Vote save(@RequestBody Vote vote) {
+		return voteRepo.save(vote);
+	}
+	
 }
--- src/main/java/dev/top/repos/VoteRepo.java
+++ src/main/java/dev/top/repos/VoteRepo.java
@@ -3,7 +3,8 @@ package dev.top.repos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.top.entities.Collegue;
+import dev.top.entities.Vote;
 
-public interface VoteRepo extends JpaRepository <Collegue, Integer> {
+public interface VoteRepo extends JpaRepository <Vote, Integer> {
 
 }
